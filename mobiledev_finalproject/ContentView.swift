85a990976516267016a1c8cb0f87a50a10afdc57
AnnaHoerschgen <102757177+AnnaHoerschgen@users.noreply.github.com>
2025-04-30 08:21:17 -0500

    Initial Commit

---
+++ mobiledev_finalproject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mobiledev_finalproject
+//
+//  Created by HOERSCHGEN, ANNA M. on 4/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mobiledev_finalproject/mobiledev_finalprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobiledev_finalprojectApp.swift
+//  mobiledev_finalproject
+//
+//  Created by HOERSCHGEN, ANNA M. on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct mobiledev_finalprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
