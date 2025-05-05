d2e568018472ef2ab5ea27d9ea176f85512dc6a4
AnnaHoerschgen <102757177+AnnaHoerschgen@users.noreply.github.com>
2025-05-05 09:30:16 -0500

    Updated so that errors regarding "searchTerm" no longer exist

--- mobiledev_finalproject/PokemonRow.swift
+++ mobiledev_finalproject/PokemonRow.swift
@@ -8,39 +8,11 @@
 import SwiftUI
 
 struct PokemonRow: View {
-    let pokemon: PokemonResponse
-    @ObservedObject var viewModel: PokemonViewModel
-
     var body: some View {
-        HStack {
-            // Pokemon Image
-            PokemonImageView(spriteURL: pokemon.sprites.defaultFrontMale)
-
-            VStack(alignment: .leading, spacing: 4) {
-                Text(pokemon.name)
-                    .font(.headline)
-                    .foregroundColor(Color(hex: "#FF5C5C")) // Red color for name
-                Text("Dex #\(pokemon.nationalDexNumber)")
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-                Text(pokemon.types.joined(separator: ", "))
-                    .font(.footnote)
-                    .foregroundColor(Color(hex: "#4DA6FF")) // Blue color for types
-            }
-
-            Spacer()
-
-            // Favorite Button
-            Button(action: {
-                viewModel.toggleFavorite(pokemon)
-            }) {
-                Image(systemName: "star.fill")
-                    .foregroundColor(viewModel.favoritePokemons.contains(where: { $0.id == pokemon.id }) ? .yellow : .gray)
-            }
-            .padding(.trailing)
-        }
-        .padding(.vertical, 6)
-        .background(Color(hex: "#2C324C")) // Darker Blue background for row
-        .cornerRadius(10)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+}
+
+#Preview {
+    PokemonRow()
 }
